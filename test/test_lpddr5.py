3111ea3b9ad5fb747e14bd74109f9913858a837b
Jędrzej Boczar <jboczar@antmicro.com>
2021-05-31 14:20:58 +0200

    phy/lpddr5: add tests for basic serialization of CK/CS/CA

--- test/test_lpddr5.py
+++ test/test_lpddr5.py
@@ -19,7 +19,9 @@ CLOCKS = {
     "sys":          (64, 31),
     "sys2x":        (32, 15),
     "sys4x":        (16,  7),
+    "sys4x_ddr":    ( 8,  3), # = sys8x
     "sys4x_90":     (16,  3),
+    "sys4x_90_ddr": ( 8,  7),
     "sys8x":        ( 8,  3),
     "sys8x_ddr":    ( 4,  1),
     "sys8x_90":     ( 8,  1),
@@ -53,13 +55,61 @@ class LPDDR5Tests(unittest.TestCase):
 
     def test_lpddr5_cs_phase_0(self):
         # Test that CS is serialized correctly when sending command on phase 0
-        self.run_test(LPDDR5SimPHY(sys_clk_freq=self.SYS_CLK_FREQ),
+        phy = LPDDR5SimPHY(sys_clk_freq=self.SYS_CLK_FREQ)
+        latency = '0000' + '0' * phy.ser_latency.sys4x
+        self.run_test(phy,
             dfi_sequence = [
-                {0: dict(cs_n=0, cas_n=0, ras_n=1, we_n=1)},  # p0: READ
+                {0: dict(cs_n=0, cas_n=1, ras_n=0, we_n=1)},  # p0: ACT
+                {0: dict(cs_n=0, cas_n=1, ras_n=0, we_n=0)},  # p0: PRE
             ],
             pad_checkers = {"sys4x_90": {
-                'cs': '0000 0000 1100',
+                'cs': latency + '1100 0100',
             }},
-            vcd_name='sim.vcd'
+        )
+
+    def test_lpddr5_ck(self):
+        # Test clock serialization, first few cycles are undefined so ignore them
+        phy = LPDDR5SimPHY(sys_clk_freq=self.SYS_CLK_FREQ)
+        latency = 'x' * (8 + phy.ser_latency.sys8x)
+        self.run_test(phy,
+            dfi_sequence = [
+                {3: dict(cs_n=0, cas_n=0, ras_n=1, we_n=1)},
+            ],
+            pad_checkers = {"sys4x_90_ddr": {
+                'ck': latency + '01010101' * 3,
+            }},
+        )
+
+    def test_lpddr5_cs_multiple_phases(self):
+        # Test that CS is serialized on different phases and that overlapping commands are handled
+        phy = LPDDR5SimPHY(sys_clk_freq=self.SYS_CLK_FREQ)
+        latency = '0' * (4 + phy.ser_latency.sys4x)
+        self.run_test(phy,
+            dfi_sequence = [
+                {0: dict(cs_n=0, cas_n=1, ras_n=0, we_n=0)},  # PRE (1 command)
+                {1: dict(cs_n=0, cas_n=1, ras_n=0, we_n=1)},  # ACT (2 commands)
+                {
+                    0: dict(cs_n=0, cas_n=1, ras_n=0, we_n=1),
+                    1: dict(cs_n=0, cas_n=1, ras_n=0, we_n=1),  # should be ignored
+                },
+                {
+                    0: dict(cs_n=0, cas_n=1, ras_n=0, we_n=1),
+                    2: dict(cs_n=0, cas_n=1, ras_n=0, we_n=1),  # should NOT be ignored
+                },
+                {3: dict(cs_n=0, cas_n=1, ras_n=0, we_n=1)},  # crosses cycle boundaries
+                {0: dict(cs_n=0, cas_n=0, ras_n=1, we_n=1)},  # should be ignored
+                {2: dict(cs_n=1, cas_n=0, ras_n=1, we_n=1)},  # ignored due to cs_n=1
+            ],
+            pad_checkers = {"sys4x_90": {
+                'cs': latency + ''.join([
+                    '0100',  # p0
+                    '0110',  # p1
+                    '1100',  # p0, p1 ignored
+                    '1111',  # p0, p2 not ignored
+                    '0001',  # p3
+                    '1000',  # p0 ignored
+                    '0000',  # p2 ignored
+                ])
+            }},
         )
 
