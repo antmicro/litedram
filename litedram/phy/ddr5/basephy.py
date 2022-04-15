c53ef4f971c7cac2b15a8dfe06fe2bdd5b96234a
Alessandro Comodi <acomodi@antmicro.com>
2022-04-15 11:19:15 +0200

    ddr5: changes to CL and CLW
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- litedram/phy/ddr5/basephy.py
+++ litedram/phy/ddr5/basephy.py
@@ -97,17 +97,19 @@ class DDR5PHY(Module, AutoCSR):
         def get_cl_cw(memtype, tck):
             # MT53E256M16D1, No DBI, Set A
             f_to_cl_cwl = OrderedDict()
-            f_to_cl_cwl[ 532e6] = ( 6,  4)
-            f_to_cl_cwl[1066e6] = (10,  6)
-            f_to_cl_cwl[1600e6] = (14,  8)
-            f_to_cl_cwl[2132e6] = (20, 10)
-            f_to_cl_cwl[2666e6] = (24, 12)
-            f_to_cl_cwl[3200e6] = (28, 14)
-            f_to_cl_cwl[3732e6] = (32, 16)
-            f_to_cl_cwl[4266e6] = (36, 18)
-            for f, (cl, cwl) in f_to_cl_cwl.items():
-                if tck >= 2/f:
-                    return cl, cwl
+            f_to_cl_cwl[3200e6] = 22
+            f_to_cl_cwl[3600e6] = 28
+            f_to_cl_cwl[4000e6] = 32
+            f_to_cl_cwl[4400e6] = 36
+            f_to_cl_cwl[4800e6] = 40
+            f_to_cl_cwl[5200e6] = 42
+            f_to_cl_cwl[5600e6] = 46
+            f_to_cl_cwl[6000e6] = 50
+            f_to_cl_cwl[6400e6] = 54
+            f_to_cl_cwl[6800e6] = 56
+            for f, cl in f_to_cl_cwl.items():
+                if tck > 1/f:
+                    return cl
             raise ValueError
 
         # Bitslip introduces latency from 1 up to `cycles + 1`
@@ -120,7 +122,8 @@ class DDR5PHY(Module, AutoCSR):
         # Commands read from adapters are delayed on ConstBitSlips
         ca_latency      = 1
 
-        cl, cwl         = get_cl_cw(memtype, tck)
+        cl              = get_cl_cw(memtype, tck)
+        cwl = cl - 2
         cl_sys_latency  = get_sys_latency(nphases, cl)
         cwl_sys_latency = get_sys_latency(nphases, cwl)
         # For reads we need to account for ser+des latency to make sure we get the data in-phase with sys clock
