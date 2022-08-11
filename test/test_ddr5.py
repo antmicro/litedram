eed540c5b07605f68465b63c3eb77f967c1418fc
Michal Sieron <msieron@antmicro.com>
2022-08-11 19:40:43 +0200

    Add CRG Add setUp method
    I looked at existing test cases and noticed that many variables with the
    same value are being defined in every test.
    
    I created `setUp` method which prepares necessary common values to use in
    tests which will make it easier to test changes while still having
    confidence, that all test were updated.
    
    It also creates `DDR5SimPhy` object before each test, so there will be
    no need to create the same thing in every test.
    
    This enables us to remove `dut` parameter from `run_test` method, as it
    is now under `self.phy`.
    
    Almost certainly all test will error out after this change, but as they
    error now anyway, I have no problem with it.
    
    Also defined parameters like databits, burst length and phase number.
    
    Signed-off-by: Michal Sieron <msieron@antmicro.com>

--- test/test_ddr5.py
+++ test/test_ddr5.py
@@ -21,14 +21,6 @@ import test.phy_common
 from test.phy_common import DFISequencer, PadChecker
 
 
-# Migen simulator supports reset signals so we could add CRG to start all the signals
-# in the same time, however the clock signals will still be visible in the VCD dump
-# and the generators we assign to them will still work before reset. For this reason we
-# use clocks set up in such a way that we have all the phase aligned clocks start in tick
-# 1 (not zero), so that we avoid any issues with clock alignment.
-#
-# NOTE: On hardware proper reset must be ensured!
-#
 # The simulation should start like this (each char is 1 ns):
 #   sys          |_--------------------------------
 #   sys2x        |_----------------________________
@@ -41,15 +33,17 @@ from test.phy_common import DFISequencer, PadChecker
 # sys4x_90_ddr does not trigger at the simulation start (not an edge),
 # BUT a generator starts before first edge, so a `yield` is needed to wait until the first
 # rising edge!
-run_simulation = partial(test.phy_common.run_simulation, clocks={
+sim_clocks={
     "sys":          (64, 31),
+    "sys_rst":      (64, 30),
     "sys2x":        (32, 15),
     "sys4x":        (16,  7),
     "sys4x_ddr":    ( 8,  3),
     "sys4x_90":     (16,  3),
     "sys4x_90_ddr": ( 8,  7),
     "sys4x_180":    (16, 15),
-})
+}
+run_simulation = partial(test.phy_common.run_simulation, clocks=sim_clocks)
 
 dfi_data_to_dq = partial(test.phy_common.dfi_data_to_dq, databits=8, nphases=4, burst=8)
 dq_pattern = partial(test.phy_common.dq_pattern, databits=8, nphases=4, burst=8)
@@ -57,10 +51,38 @@ dq_pattern = partial(test.phy_common.dq_pattern, databits=8, nphases=4, burst=8)
 
 class DDR5Tests(unittest.TestCase):
     SYS_CLK_FREQ = 50e6
+    DATABITS = 8
+    BURST_LENGTH = 8
+    NPHASES = 4
 
-    def run_test(self, dut, dfi_sequence, pad_checkers: Mapping[str, Mapping[str, str]], pad_generators=None, **kwargs):
+    def setUp(self):
+        self.phy = DDR5SimPHY(sys_clk_freq=self.SYS_CLK_FREQ, aligned_reset_zero=True)
+
+        self.rdphase: int = self.phy.settings.rdphase.reset.value
+        self.wrphase: int = self.phy.settings.wrphase.reset.value
+
+        self.cmd_latency:   int = self.phy.settings.cmd_latency
+        self.read_latency:  int = self.phy.settings.read_latency
+        self.write_latency: int = self.phy.settings.write_latency
+
+        # 0s, 1s and Xs for 1 sys_clk in `*_ddr` clock domain
+        self.zeros: str = '0' * self.NPHASES * 2
+        self.ones:  str = '1' * self.NPHASES * 2
+        self.xs:    str = 'x' * self.NPHASES * 2
+
+        # latencies to use in pad checkers
+        self.ca_latency:       str = self.xs + '0' * self.NPHASES + '0' * self.NPHASES * self.cmd_latency
+        self.cs_n_latency:     str = self.xs + '0' * self.NPHASES + '1' * self.NPHASES * self.cmd_latency
+
+        self.dqs_t_rd_latency: str = self.xs * 2 + (self.read_latency - 2 - 1) * self.xs + 'x' * (self.NPHASES - 1) * 2
+        self.dq_rd_latency:    str = self.xs * 2 + (self.read_latency - 2) * self.zeros  + '0' * (self.NPHASES - 1) * 2
+        self.dqs_t_wr_latency: str = self.xs * 2 + (self.cmd_latency + self.write_latency - 1) * self.xs + 'x' * (self.NPHASES - 1) * 2
+        self.dq_wr_latency:    str = self.xs * 2 + (self.cmd_latency + self.write_latency) * self.zeros  + '0' * (self.NPHASES - 1) * 2
+
+    def run_test(self, dfi_sequence, pad_checkers: Mapping[str, Mapping[str, str]], pad_generators=None, **kwargs):
         # pad_checkers: {clock: {sig: values}}
-        dfi = DFISequencer(dfi_sequence)
+        dut = self.phy
+        dfi = DFISequencer([{}, {}] + dfi_sequence)
         checkers = {clk: PadChecker(dut.pads, pad_signals) for clk, pad_signals in pad_checkers.items()}
         generators = defaultdict(list)
         generators["sys"].append(dfi.generator(dut.dfi))
@@ -72,6 +94,19 @@ class DDR5Tests(unittest.TestCase):
             gens = gens if isinstance(gens, list) else [gens]
             for gen in gens:
                 generators[clock].append(gen(dut.pads))
+
+        class CRG(Module):
+            def __init__(self, dut):
+                r = Signal(2)
+                self.sync.sys_rst += [If(r<3, r.eq(r+1))]
+                self.submodules.dut = dut
+                for clk in sim_clocks:
+                    if clk == "sys_rst":
+                        continue
+                    setattr(self.clock_domains, "cd_{}".format(clk), ClockDomain(clk))
+                    cd = getattr(self, 'cd_{}'.format(clk))
+                    self.comb += cd.rst.eq(~r[1])
+        dut = CRG(dut)
         run_simulation(dut, generators, **kwargs)
         PadChecker.assert_ok(self, checkers)
         dfi.assert_ok(self)
