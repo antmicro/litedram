b7c5d2851737614b1f62153422f9dd2a6e04037d
Maciej Dudek <mdudek@antmicro.com>
2022-11-15 21:54:59 +0100

    phy/ddr5/basephy.py: Add default write latency to BasePHY argument list phy/ddr5/basephy.py: Store minimal PHY read and write latencies to phy_settings
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litedram/phy/ddr5/basephy.py
+++ litedram/phy/ddr5/basephy.py
@@ -143,7 +143,8 @@ class DDR5PHY(Module, AutoCSR):
                  sys_clk_freq, ser_latency, des_latency, phytype, with_sub_channels=False,
                  cmd_delay=None, masked_write=False, extended_overlaps_check=False,
                  with_odelay=False, with_clock_odelay=False, with_address_odelay=False,
-                 csr_cdc=None, rd_extra_delay=Latency(sys=0), default_read_latency=0):
+                 csr_cdc=None, rd_extra_delay=Latency(sys=0),
+                 default_read_latency=0, default_write_latency=0):
 
         self.pads        = pads
         self.memtype     = memtype     = "DDR5"
@@ -226,8 +227,7 @@ class DDR5PHY(Module, AutoCSR):
 
         for prefix in prefixes:
             setattr(self, prefix+'wlevel_en', CSRStorage(name=prefix+'wlevel_en'))
-            setattr(self, prefix+'wtodqsdl', CSRStorage(6, name=prefix+'wtodqsdl', reset=0))
-
+            setattr(self, prefix+'wtodqsdl', CSRStorage(6, name=prefix+'wtodqsdl', reset=default_write_latency))
             setattr(self, prefix+'rtodatadl', CSRStorage(6, name=prefix+'rtodatadl', reset=default_read_latency))
 
             setattr(self, prefix+'dly_sel', CSRStorage(max(strobes, databits, 14, nranks), name=prefix+'dly_sel'))
@@ -310,7 +310,9 @@ class DDR5PHY(Module, AutoCSR):
             cmd_delay     = cmd_delay,
             bitslips      = 8,
             strobes       = combined_strobes,
-            with_sub_channels  = with_sub_channels,
+            min_write_latency   = min_write_latency,
+            min_read_latency    = min_read_latency,
+            with_sub_channels   = with_sub_channels,
         )
 
         # DFI Interface ----------------------------------------------------------------------------
