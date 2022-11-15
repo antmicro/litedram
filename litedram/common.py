baa3c67e3d4489eed2757bab9ad10c74584e9d49
Maciej Dudek <mdudek@antmicro.com>
2022-11-15 21:46:26 +0100

    common.py: Add fields for minimal PHY delay, used by phy with dynamic DQS/DQ delays
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litedram/common.py
+++ litedram/common.py
@@ -225,6 +225,8 @@ class PhySettings(Settings):
             cmd_delay: Optional[int] = None,  # used to force cmd delay during initialization in BIOS
             bitslips: int = 0,  # number of write/read bitslip taps
             delays: int = 0,  # number of write/read delay taps
+            min_write_latency: int = 0,
+            min_read_latency: int = 0,
             # PHY training capabilities
             write_leveling: bool = False,
             write_dq_dqs_training: bool = False,
