3b92638d4410e5c60f1ff2cc29dc4ffa6548b71e
Maciej Dudek <mdudek@antmicro.com>
2023-01-12 11:52:05 +0100

    phy/ddr5/simsoc.py: Skip parts of training in simulation
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litedram/phy/ddr5/simsoc.py
+++ litedram/phy/ddr5/simsoc.py
@@ -343,6 +343,7 @@ class SimSoC(SoCCore):
         # Reduce memtest size for simulation speedup
         self.add_constant("MEMTEST_DATA_SIZE", 8*1024)
         self.add_constant("MEMTEST_ADDR_SIZE", 8*1024)
+        self.add_constant("DDR5_TRAINING_SIM", 1)
 
         # DDR5 Sim -------------------------------------------------------------------------------
         prefixes = [""] if not with_sub_channels else ["A_", "B_"]
