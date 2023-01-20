d67472f7d75e06fef98e5f3fe8fd1718d058e1bf
Maciej Dudek <mdudek@antmicro.com>
2023-01-20 18:53:09 +0100

    litedram/phy/ddr5/simsoc.py: Add default_write_latency
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litedram/phy/ddr5/simsoc.py
+++ litedram/phy/ddr5/simsoc.py
@@ -200,6 +200,9 @@ class SimSoC(SoCCore):
             masked_write       = masked_write,
             dq_dqs_ratio       = dq_dqs_ratio,
             with_sub_channels  = with_sub_channels
+            with_sub_channels  = with_sub_channels,
+            address_lines      = sdram_module.address_bits,
+            default_write_latency   = 20,
         )
 
         for p in _io[io_type][0][2:]:
