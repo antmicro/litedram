68fe6530950f56655d8f914094eda7ba7f1d96b6
Maciej Dudek <mdudek@antmicro.com>
2023-01-27 14:53:57 +0100

    Increase timeouts for lpddr4, lpddr5 and ddr5 verilator tests
    Some CI runs go green and some CI runs get timeouts on these tests.
    I increased the timeout to 30 minutes per test for lpddr memories
    and to 3 hours for ddr5. This should be well above current run time.
    
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- test/test_lpddr4.py
+++ test/test_lpddr4.py
@@ -676,7 +676,7 @@ class VerilatorLPDDR4Tests(unittest.TestCase):
         import pexpect
 
         command = ["python3", simsoc.__file__, *args]
-        timeout = 12 * 60  # give more than enough time
+        timeout = 30 * 60  # give more than enough time
         p = pexpect.spawn(" ".join(command), timeout=timeout, **kwargs)
 
         res = p.expect(["Memtest OK", "Memtest KO"])
