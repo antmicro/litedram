8027a1d09b6307c41dc4cf17e21c4859e57539f7
Michal Czyz <mczyz@antmicro.com>
2023-01-27 13:38:03 +0100

    Refactor test to enable usage of environment and monitor

--- litedram/DDR5RCD01/unittests/test_core.py
+++ litedram/DDR5RCD01/unittests/test_core.py
@@ -17,6 +17,9 @@ from litedram.DDR5RCD01.RCD_interfaces import *
 from litedram.DDR5RCD01.RCD_interfaces_external import *
 # Submodules
 from litedram.DDR5RCD01.DDR5RCD01Core import DDR5RCD01Core
+from litedram.DDR5RCD01.BusCSCAEnvironment import BusCSCAEnvironment
+from litedram.DDR5RCD01.BusCSCAEnvironment import EnvironmentScenarios
+from litedram.DDR5RCD01.BusCSCAMonitor import BusCSCAMonitor
 
 
 class TestBed(Module):
@@ -25,7 +28,8 @@ class TestBed(Module):
         self.if_sdram_A = If_sdram()
         self.if_sdram_B = If_sdram()
         self.if_alert_n = If_alert_n()
-        self.if_ibuf = If_ibuf()
+        self.if_ibuf_A = If_ibuf()
+        self.if_ibuf_B = If_ibuf()
         self.if_obuf_A = If_obuf()
         self.if_obuf_B = If_obuf()
         self.if_lb = If_lb()
@@ -34,12 +38,21 @@ class TestBed(Module):
         self.if_sideband = If_sideband()
         self.is_dual_channel = is_dual_channel
 
-        self.submodules.dut = DDR5RCD01Core(
+        self.submodules.xenvironment = BusCSCAEnvironment(
+            if_ibuf_o=self.if_ibuf_A,
+        )
+
+        self.submodules.xmonitor_ingress = BusCSCAMonitor(
+            if_ibuf_i=self.if_ibuf_A
+        )
+
+        self.submodules.xrcd_core = DDR5RCD01Core(
             if_ck_rst=self.if_ck_rst,
             if_sdram_A=self.if_sdram_A,
             if_sdram_B=self.if_sdram_B,
             if_alert_n=self.if_alert_n,
-            if_ibuf=self.if_ibuf,
+            if_ibuf_A=self.if_ibuf_A,
+            if_ibuf_B=self.if_ibuf_B,
             if_obuf_A=self.if_obuf_A,
             if_obuf_B=self.if_obuf_B,
             if_lb=self.if_lb,
@@ -49,82 +62,14 @@ class TestBed(Module):
             is_dual_channel=self.is_dual_channel,
         )
 
-    def n_ui_dram_command(self, nums, non_target_termination=False):
-        """
-        This function drives the interface with as in:
-            "JEDEC 82-511 Figure 7
-            One UI DRAM Command Timing Diagram"
+        self.submodules.xmonitor_egress = BusCSCAMonitor(
+            if_ibuf_i=self.if_obuf_A
+        )
 
-        Nums can be any length to incroporate two, or more, UI commands
-
-        The non target termination parameter extends the DCS assertion to the 2nd UI
-        """
-        SEQ_INACTIVE = [~0, 0]
-        yield from self.drive_init()
-
-        sequence = [SEQ_INACTIVE]
-        for id, num in enumerate(nums):
-            if non_target_termination:
-                if id in [0, 1, 2, 3]:
-                    sequence.append([0b00, num])
-                else:
-                    sequence.append([0b11, num])
-            else:
-                if id in [0, 1]:
-                    sequence.append([0b00, num])
-                else:
-                    sequence.append([0b11, num])
-
-        sequence.append(SEQ_INACTIVE)
-
-        for seq_cs, seq_ca in sequence:
-            logging.debug(str(seq_cs) + " " + str(seq_ca))
-            yield from self.drive_cs_ca(seq_cs, seq_ca)
-        for i in range(3):
-            yield
-
-    def seq_cmds(self):
-        # TODO all commands are passed as if they were 2UIs long. To be fixed.
-        # Single UI command
-        yield from self.n_ui_dram_command(nums=[0x01, 0x02])
-        # 2 UI commands
-        yield from self.n_ui_dram_command(nums=[0x01, 0x02, 0x03, 0x04])
-        yield from self.n_ui_dram_command(nums=[0x0A, 0x0B, 0x0C, 0x0D], non_target_termination=True)
-        yield from self.n_ui_dram_command(nums=[0xDE, 0xAD, 0xBA, 0xBE], non_target_termination=True)
-        yield from self.n_ui_dram_command(nums=[0xC0, 0xDE, 0xF0, 0x0D])
-
-    def drive_init(self):
-        yield self.if_ck_rst.drst_n.eq(1)
-        yield from self.drive_cs_ca(~0, 0)
-
-    def drive_cs_ca(self, cs, ca):
-        yield self.if_ibuf.dcs_n.eq(cs)
-        yield self.if_ibuf.dca.eq(ca)
-        yield
-
-    def scenario_compile(self):
-        for i in range(2):
-            yield
-
-    def scenario_init_few_commands(self):
-        INIT_CYCLES = CW_DA_REGS_NUM + 5
-        yield from self.drive_init()
-        for i in range(INIT_CYCLES):
-            yield
-        yield from self.seq_cmds()
-        for i in range(3):
-            yield
-
-    def scenario_clock_distribution(self):
-        INIT_CYCLES = CW_DA_REGS_NUM + 5
-        yield from self.drive_init()
-        for i in range(INIT_CYCLES):
-            yield
-
-        for b in [0, 1]*5:
-            yield self.if_ck_rst.dck_t.eq(b)
-            yield self.if_ck_rst.dck_c.eq(~b)
-            yield
+    def scenario(self):
+        scenario_select = EnvironmentScenarios.SIMPLE_GENERIC
+        yield from self.xenvironment.run_env(scenario_select=scenario_select)
+        yield from self.xmonitor_ingress.post_process()
 
 
 class DDR5RCD01CoreTests_SingleChannel(unittest.TestCase):
@@ -137,44 +82,13 @@ class DDR5RCD01CoreTests_SingleChannel(unittest.TestCase):
         file_name = self._testMethodName
         self.wave_file_name = dir_name + '/' + file_name + ".vcd"
 
-    def tearDown(self):
-        del self.tb
-
-    def test_core_compile(self):
-        run_simulation(self.tb,
-                       self.tb.scenario_compile(),
-                       vcd_name=self.wave_file_name
-                       )
-
-    def test_commands(self):
-        run_simulation(self.tb,
-                       self.tb.scenario_init_few_commands(),
-                       vcd_name=self.wave_file_name
-                       )
-
-
-class DDR5RCD01CoreTests_DualChannel(unittest.TestCase):
-
-    def setUp(self):
-        self.tb = TestBed(is_dual_channel=True)
-        dir_name = "./wave_dc"
-        if not os.path.exists(dir_name):
-            os.mkdir(dir_name)
-        file_name = self._testMethodName
-        self.wave_file_name = dir_name + '/' + file_name + ".vcd"
 
     def tearDown(self):
         del self.tb
 
     def test_core_compile(self):
         run_simulation(self.tb,
-                       self.tb.scenario_compile(),
-                       vcd_name=self.wave_file_name
-                       )
-
-    def test_clks(self):
-        run_simulation(self.tb,
-                       self.tb.scenario_clock_distribution(),
+                       self.tb.scenario(),
                        vcd_name=self.wave_file_name
                        )
 
