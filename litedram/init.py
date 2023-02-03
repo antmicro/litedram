074da7d8a0fbfbab2b2fa3e9efb6334f01214736
Maciej Dudek <mdudek@antmicro.com>
2023-02-03 09:39:45 +0100

    Reduce from 250 to 50 MHz, as cpus take longer than 5 cycles to process single cdealy iteration

--- litedram/init.py
+++ litedram/init.py
@@ -990,7 +990,7 @@ def get_ddr5_phy_init_sequence(phy_settings, timing_settings):
 
     def ck(sec):
         # FIXME: use sys_clk_freq (should be added e.g. to TimingSettings), using arbitrary value for now
-        fmax = 250e6 # as we perform active waiting we can assume multiple singlecycle operations
+        fmax = 50e6 # as we perform active waiting we can assume multiple singlecycle operations
         return int(math.ceil(sec * fmax))
 
     reset_sequence = [
