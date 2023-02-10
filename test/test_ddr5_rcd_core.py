29104554e86c275cc1ac05b774cd3d2a4c1a5326
Michal Czyz <mczyz@antmicro.com>
2023-02-10 17:26:19 +0100

    Fix test method name to contain ddr5

--- test/test_ddr5_rcd_core.py
+++ test/test_ddr5_rcd_core.py
@@ -183,7 +183,7 @@ class DDR5RCD01CoreTests_SingleChannel(unittest.TestCase):
     def tearDown(self):
         del self.tb
 
-    def test_mode_normal_rank_A(self):
+    def test_ddr5_mode_normal_rank_A(self):
         logger = logging.getLogger('root')
 
         """
