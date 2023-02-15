e20ccc19041d0dcc0a343266f66cde3debe1634d
Michal Sieron <msieron@antmicro.com>
2023-02-15 14:07:57 +0100

    Implement I2CMockSlave
    Its FSM is needed as page number must be set before performing operations
    on actual registers and I2CMockMaster sends commands containing both
    page_num and reg_num.
    
    Signed-off-by: Michal Sieron <msieron@antmicro.com>

--- litedram/DDR5RCD01/I2CMockSlave.py
+++ litedram/DDR5RCD01/I2CMockSlave.py
@@ -12,6 +12,10 @@ from litedram.DDR5RCD01.RCD_interfaces import *
 from litedram.DDR5RCD01.RCD_interfaces_external import *
 from litedram.DDR5RCD01.RCD_utils import *
 
+# JESD82-511 Table 42
+CHANNEL_A_ADDRESS = 0b0000
+CHANNEL_B_ADDRESS = 0b0001
+
 class I2CMockSlave(Module):
     """
         I2C Mock Slave
@@ -21,8 +25,55 @@ class I2CMockSlave(Module):
 
         TODO Implementation: a parallel interface based on chapter 7 JEDEC spec
     """
-    def __init__(self):
-        pass
+    def __init__(self, if_mock: If_sideband_mock, if_regs_A: If_registers, if_regs_B: If_registers):
+        channel = Signal(4)
+        page_num = Signal(8)
+        reg_num = Signal(8)
+        data = Signal(8)
+
+        self.submodules.fsm = fsm = FSM()
+
+        fsm.act("IDLE",
+            If(if_mock.we,
+                NextValue(data, if_mock.data),
+                NextValue(page_num, if_mock.page_num),
+                NextValue(reg_num, if_mock.reg_num),
+                NextValue(channel, if_mock.channel),
+                NextState("SET_PAGE"),
+            ),
+        )
+
+        fsm.act("SET_PAGE",
+            Case(channel, {
+                CHANNEL_A_ADDRESS: [
+                    if_regs_A.we.eq(1),
+                    if_regs_A.d.eq(page_num),
+                    if_regs_A.addr.eq(ADDR_CW_PAGE),
+                ],
+                CHANNEL_B_ADDRESS: [
+                    if_regs_B.we.eq(1),
+                    if_regs_B.d.eq(page_num),
+                    if_regs_B.addr.eq(ADDR_CW_PAGE),
+                ]
+            }),
+            NextState("WRITE"),
+        )
+
+        fsm.act("WRITE",
+            Case(channel, {
+                CHANNEL_A_ADDRESS: [
+                    if_regs_A.we.eq(1),
+                    if_regs_A.d.eq(data),
+                    if_regs_A.addr.eq(reg_num),
+                ],
+                CHANNEL_B_ADDRESS: [
+                    if_regs_B.we.eq(1),
+                    if_regs_B.d.eq(data),
+                    if_regs_B.addr.eq(reg_num),
+                ]
+            }),
+            NextState("IDLE"),
+        )
 
 
 if __name__ == "__main__":
