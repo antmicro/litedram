b8857ed50eeace2d07661d9e11e17975ba6bb6fe
Michal Czyz <mczyz@antmicro.com>
2023-02-16 15:50:07 +0100

    Implement and test MRW/MRR actor

--- litedram/DDR5RCD01/DDR5RCD01ActorMRW.py
+++ litedram/DDR5RCD01/DDR5RCD01ActorMRW.py
@@ -24,8 +24,9 @@ class RCDSpecialMR(enum.IntEnum):
     MRW_3F = 0x3F
 
 
-class RCDSpecialOpcode(enum.IntEnum):
-    MRW = 0b00101
+# class RCDSpecialOpcode(enum.IntEnum):
+#     MRW = 0b00101
+#     MRR = 0b00101
 
 
 class DDR5RCD01ActorMRW(Module):
@@ -52,7 +53,11 @@ class DDR5RCD01ActorMRW(Module):
 
     def __init__(self,
                  if_csca_i,
+                 if_csca_i_rank_A,
+                 if_csca_i_rank_B,
                  if_csca_o,
+                 if_csca_o_rank_A,
+                 if_csca_o_rank_B,
                  valid,
                  is_this_ui_odd,
                  is_cmd_beginning,
@@ -67,7 +72,8 @@ class DDR5RCD01ActorMRW(Module):
 
         is_mrw_detected = Signal()
         mra = Signal(CW_REG_BIT_SIZE)
-        opcode = Signal(CW_REG_BIT_SIZE)
+        op = Signal(CW_REG_BIT_SIZE)
+        op_override = Signal(CW_REG_BIT_SIZE)
         cw_bit = Signal()
 
         delay_len = 4
@@ -81,7 +87,7 @@ class DDR5RCD01ActorMRW(Module):
         self.sync += If(
             valid & is_cmd_beginning,
             If(
-                if_csca_i.dca[0:5] == RCDSpecialOpcode.MRW,
+                if_csca_i.dca[0:5] == DDR5Opcodes.MRW,
                 is_mrw_detected.eq(1),
                 mra[0:2].eq(if_csca_i.dca[5:7]),
             ).Else(
@@ -96,14 +102,73 @@ class DDR5RCD01ActorMRW(Module):
 
         self.sync += If(
             valid & is_cmd_beginning_d[1],
-            opcode[0:7].eq(if_csca_i.dca[0:7]),
+            op[0:7].eq(if_csca_i.dca[0:7]),
         )
 
         self.sync += If(
             valid & is_cmd_beginning_d[2],
-            opcode[7].eq(if_csca_i.dca[0]),
+            op[7].eq(if_csca_i.dca[0]),
             cw_bit.eq(if_csca_i.dca[3]),
         )
+        """
+            MRW
+        """
+        self.sync += If(
+            cw_bit & is_cmd_beginning_d[3],
+            reg_q.eq(op),
+            reg_addr.eq(mra),
+            reg_we.eq(1),
+        ).Else(
+            reg_q.eq(0),
+            reg_addr.eq(0),
+            reg_we.eq(0),
+        )
+
+        """
+            MRW to DRAM 0x3F
+        """
+        self.comb += If(
+            (~cw_bit) & (mra == RCDSpecialMR.MRW_3F) & is_cmd_beginning_d[1],
+            if_csca_o.dca[0:7].eq(reg_d[0:7]),
+        ).Elif(
+            (~cw_bit) & (mra == RCDSpecialMR.MRW_3F) & is_cmd_beginning_d[2],
+            if_csca_o.dca[0].eq(reg_d[7]),
+            if_csca_o.dca[1:7].eq(if_csca_i.dca[1:7])
+        ).Else(
+            if_csca_o.dca.eq(if_csca_i.dca),
+        )
+
+        self.comb += If(
+            (~cw_bit) & (mra == RCDSpecialMR.MRW_3F) & is_cmd_beginning_d[1],
+            if_csca_o_rank_A.dca[0:7].eq(reg_d[0:7]),
+        ).Elif(
+            (~cw_bit) & (mra == RCDSpecialMR.MRW_3F) & is_cmd_beginning_d[2],
+            if_csca_o_rank_A.dca[0].eq(reg_d[7]),
+            if_csca_o_rank_A.dca[1:7].eq(if_csca_i_rank_A.dca[1:7])
+        ).Else(
+            if_csca_o_rank_A.dca.eq(if_csca_i_rank_A.dca),
+        )
+
+        self.comb += If(
+            (~cw_bit) & (mra == RCDSpecialMR.MRW_3F) & is_cmd_beginning_d[1],
+            if_csca_o_rank_B.dca[0:7].eq(reg_d[0:7]),
+        ).Elif(
+            (~cw_bit) & (mra == RCDSpecialMR.MRW_3F) & is_cmd_beginning_d[2],
+            if_csca_o_rank_B.dca[0].eq(reg_d[7]),
+            if_csca_o_rank_B.dca[1:7].eq(if_csca_i_rank_B.dca[1:7])
+        ).Else(
+            if_csca_o_rank_B.dca.eq(if_csca_i_rank_B.dca),
+        )
+
+        self.comb += if_csca_o.dpar.eq(if_csca_i.dpar)
+        self.comb += if_csca_o.dcs_n.eq(if_csca_i.dcs_n)
+
+        self.comb += if_csca_o_rank_A.dpar.eq(if_csca_i_rank_A.dpar)
+        self.comb += if_csca_o_rank_A.dcs_n.eq(if_csca_i_rank_A.dcs_n)
+
+        self.comb += if_csca_o_rank_B.dpar.eq(if_csca_i_rank_B.dpar)
+        self.comb += if_csca_o_rank_B.dcs_n.eq(if_csca_i_rank_B.dcs_n)
+
 
 
 class TestBed(Module):
