f55117ca2c525ee488b676214903b4e5c7a1d8af
Maciej Dudek <mdudek@antmicro.com>
2023-04-04 00:09:25 +0200

    Set default reset state to on

--- litedram/phy/ddr5/BasePHYCSR.py
+++ litedram/phy/ddr5/BasePHYCSR.py
@@ -15,7 +15,7 @@ class BasePHYCSR(Module, AutoCSR):
                  with_idelay, with_odelay,
                  with_per_dq_idelay, databits, dq_dqs_ratio):
         self._enable_fifos = CSRStorage(reset=0)
-        self._rst           = CSRStorage(1)
+        self._rst           = CSRStorage(reset=1)
         self._rst.storage.attr.add("slow_ff")
         self._rst.storage.attr.add("keep")
         self._rdimm_mode    = CSRStorage()
