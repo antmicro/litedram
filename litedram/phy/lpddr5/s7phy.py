5ccfcb317bfa7008038a81b2167957e7aa49a572
Maciej Dudek <mdudek@antmicro.com>
2024-03-06 15:32:07 +0100

    LPDDR5: Fix power enable signals
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litedram/phy/lpddr5/s7phy.py
+++ litedram/phy/lpddr5/s7phy.py
@@ -43,8 +43,9 @@ class S7LPDDR5PHY(LPDDR5PHY, S7Common):
         assert half_sys4x_taps < 32, "Exceeded ODELAYE2 max value: {} >= 32".format(half_sys4x_taps)
 
         # Power enable
-        self.obuf(din=1, dout=self.pads.pwr_en)
-        self.ibuf(din=self.pads.pwr_good, dout=Signal())
+        if hasattr(self.pads, "pwr_en"):
+            self.obuf(din=1, dout=self.pads.pwr_en)
+            self.ibuf(din=self.pads.pwr_good, dout=Signal())
 
         # Registers --------------------------------------------------------------------------------
         # Note: this should be named sys4x, but using sys8x due to a name hard-coded in BIOS
