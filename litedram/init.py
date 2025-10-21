28a0d2356c7c7d7066ee7529a477c3296a510358
Maciej Dudek <mdudek@antmicro.com>
2025-10-21 12:45:02 +0200

    Fix LPDDR5 size
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- litedram/init.py
+++ litedram/init.py
@@ -1337,8 +1337,6 @@ def get_sdram_phy_c_header(phy_settings, timing_settings, geom_settings):
     supported_memory = 2 ** (geom_settings.bankbits +
                         geom_settings.rowbits +
                         geom_settings.colbits) * phy_settings.databits // 8
-    if phy_settings.memtype == "LPDDR5":
-        supported_memory *= 16
     r.define("SDRAM_PHY_SUPPORTED_MEMORY", f"0x{supported_memory:016x}ULL")
 
     r.newline()
